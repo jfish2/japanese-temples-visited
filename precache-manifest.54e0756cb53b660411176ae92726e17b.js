cf9942466a2cc29da68a69c2d76e3a546a9e78da
Jon <jonfisher@lyft.com>
2020-02-18 14:10:55 -0800

    Updates

---
+++ precache-manifest.54e0756cb53b660411176ae92726e17b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65aec05a8b2bdf182e4dcd3f4b246f4b",
+    "url": "/japanese-temples-visited/index.html"
+  },
+  {
+    "revision": "68e5084d0beebb8cc7c5",
+    "url": "/japanese-temples-visited/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "b9978ecb0d476d5a2b96",
+    "url": "/japanese-temples-visited/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "68e5084d0beebb8cc7c5",
+    "url": "/japanese-temples-visited/static/js/2.7b0a8e1a.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/japanese-temples-visited/static/js/2.7b0a8e1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9978ecb0d476d5a2b96",
+    "url": "/japanese-temples-visited/static/js/main.9f945acc.chunk.js"
+  },
+  {
+    "revision": "fcc8bd7b8aef120789dc",
+    "url": "/japanese-temples-visited/static/js/runtime-main.99e0c165.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/japanese-temples-visited/precache-manifest.54e0756cb53b660411176ae92726e17b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/japanese-temples-visited/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
